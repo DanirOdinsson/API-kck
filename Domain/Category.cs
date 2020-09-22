11be9f4d6450d122b67da8cd2dc5767f831852d1
Kenwyr <39334020+Kenwyr@users.noreply.github.com>
2020-09-22 13:26:17 +0200

    13,26

---
+++ Domain/Category.cs
@@ -0,0 +1,8 @@
+namespace Domain
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string type { get; set; }
+    }
+}
--- Domain/OrderLine.cs
+++ Domain/OrderLine.cs
@@ -3,6 +3,9 @@ namespace Domain
     public class OrderLine
     {
         public int OrderLineID { get; set; }
-        public int MyProperty { get; set; }
+        public int OrderID { get; set; }
+        public int ProductID { get; set; }
+        public int Quantiiy { get; set; }
+        public int Price { get; set; }
     }
 }
